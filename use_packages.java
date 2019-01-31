fe6de70549907d20a98e4f013dfda769393ed470
Gagandeep07-Kaur <gagandeep07kaur@gmail.com>
2019-01-31 12:33:31 +0530

    packages

---
+++ use_packages.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+ public class use_packages  {
+
+    public static void main (String[]args)
+{
+    String username;
+  Scanner value = new Scanner(System.in);
+
+ System.out.println("Enter your name:");
+ username = value.nextLine();
+ System.out.println("Username is:" +username);
+}
+ }
