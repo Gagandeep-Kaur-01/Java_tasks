e3fb4f2f9ab65300d7b77029850cddb2d6a43a49
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-02-09 17:27:39 +0530

    replace values in a string

---
+++ replaceString.java
@@ -0,0 +1,17 @@
+public class replaceString {
+
+public static void main(String [] args){
+	
+// Replace() method	
+	
+ String a = "Hello! World";
+	String b = a.replace("!", ".");
+	 System.out.println(b);
+   System.out.println("I don't like these \"double\" quotes".replace("\"", ""));
+   
+
+String name = "My name is Gagandeep Kaur";
+System.out.println("Your Name :" +name);
+System.out.println("Your Name :" +name.replace("Gagandeep", " "));
+}
+}
