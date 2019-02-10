a4d61caa356885672ae7f67054b3ec108391477d
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-02-10 16:15:32 +0530

    adding replaceAll and replaceFirst methods

--- replaceString.java
+++ replaceString.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class replaceString {
 
 public static void main(String [] args){
 	
-// Replace() method	
+// replace() method	
 	
  String a = "Hello! World";
 	String b = a.replace("!", ".");
@@ -13,5 +15,34 @@ public static void main(String [] args){
 String name = "My name is Gagandeep Kaur";
 System.out.println("Your Name :" +name);
 System.out.println("Your Name :" +name.replace("Gagandeep", " "));
+
+String st = "HTML|CSS|BOOTSTRAP";
+st = st.replace('|', ',');
+System.out.println(st);
+
+
+
+// replaceAll() method
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter Source Term :");
+String source = sc.nextLine();
+
+System.out.println("Enter Search Term :");
+String search = sc.nextLine();
+search = "(?i)"+search; //(?i) to search term.
+// This is to pass regex to match strings with case insensitive way. 
+
+System.out.println("Enter Replace Term :");
+String replace = sc.nextLine();
+
+String result = source.replaceAll(search, replace);
+System.out.println("Result Term :" +result);
+
+sc.close();
+
+// replaceFirst() method
+        String str = "My Name is Gagandeep Kaur";
+		str = str.replaceFirst("(?i)"+"Gagan", "Raman");
+		System.out.println(str);
 }
 }
