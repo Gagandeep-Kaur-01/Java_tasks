7def872dc31be3b47f8174f3b66cd2df6b348d08
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-02-11 22:26:09 +0530

    another way of diagnol problem

--- diagnolDiff.java
+++ diagnolDiff.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 import java.lang.*;
 
 public class diagnolDiff {
@@ -7,8 +7,8 @@ public static void main(String args[]) {
 int r, c, i, j, d1=0, d2=0, diff=0;
 Scanner in = new Scanner(System.in);
 System.out.println("Enter the number of rows and columns:");
-r = in.nextInt();
-c = in.nextInt();
+r=c = in.nextInt();
+//c = in.nextInt();
 
 int matrix[][] = new int[r][c];
 System.out.println("Elements of matrix:");
@@ -52,6 +52,70 @@ System.out.println("Difference of sum between two diagonals are:" + Math.abs(dif
 
 
 }
-} 
+}  */
+
+// Another way hackerrank.com
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class diagnolDiff {
+
+ 
+    static int diagonalDifference(int[][] arr) {
+int d1 = 0, d2 = 0;
+    
+    
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i == j) {
+                    d1 = d1 + arr[i][j];
+                }
+
+                // to calculate the sum elements of secondary diagnol
+                if ((i + j) == (arr.length - 1)) {
+                    d2 = d2 + arr[i][j];
+                }
+
+            }  
+        
+        }
+    return Math.abs(d1 - d2);
+   }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
 
  
