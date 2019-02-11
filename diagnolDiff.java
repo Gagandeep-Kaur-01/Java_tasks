7a0c194dad80ab1cbe9fa84082f2c4115d89fee4
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-02-11 12:46:35 +0530

    calculate sum of 1 diagnol elements

--- diagnolDiff.java
+++ diagnolDiff.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class diagnolDiff {
 
 public static void main(String args[]) {
-int r, c, i, j, d1, d2;
+int r, c, i, j, d1=0, d2;
 Scanner in = new Scanner(System.in);
 System.out.println("Enter the number of rows and columns:");
 r = in.nextInt();
@@ -15,14 +15,30 @@ for(i=0; i<r; i++)
 {
 	for(j=0; j<c; j++)
 	{
-
-System.out.print("Elements of matrix["+i+"]["+j+"]:");
  matrix[i][j]=in.nextInt();
-
 	}
 }
+System.out.print("Elements of matrix :");
+for(i=0; i<r; i++)
+{
+	for(j=0;j<c;j++)
+	{
+		System.out.println(matrix[i][j]);
+	}
+}
+for(i=0;i<r;i++)
+{
+	for(j=0;j<c;j++){
+		if(i==j)
+		{
+			d1 = d1+matrix[i][j];
+		}
+	}
+}
+
+System.out.println("Sum of elements of diagnol is :" +d1);
  
- System.out.println();
+ //System.out.println();
 	
 
 }
