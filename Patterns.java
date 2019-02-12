9bdcc2832721bab1c1cfa4bfd47eaa0e8247943a
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-02-12 17:48:15 +0530

    adding another pattern

--- Patterns.java
+++ Patterns.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 
 public class Patterns {
 
 public static void main(String[] args )  {
-	
+	Scanner in = new Scanner(System.in);
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
 int a,b;
 for(a=1; a<=5; a++) 
@@ -24,6 +25,21 @@ for(int i=1; i<=5; i++)
 	}
 	System.out.println();
 }
+System.out.println("\n");
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+System.out.print("Enter your name :");
+String name = in.next();
+int n=10;
+for(int k=1; k<=n; k++){
+	for(int l=1; l<=k; l++){
+		System.out.print(name);
+	}
+	System.out.println("\n");
+}
+//System.out.println("Your name is: " + name);
+
 
 }
 }
