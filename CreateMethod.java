389fb97da10b93050117b6e97320aa6010b6cd5a
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-02-15 09:36:29 +0530

    creating and calling another method and class

--- CreateMethod.java
+++ CreateMethod.java
@@ -2,20 +2,26 @@ public class CreateMethod {
 public static void main(String [] args) {
 int a = 10;
 int b = 20;
-int c = maxvalue(a,b);   // method calling
+int c = maxvalue(a,b);               // method calling
 System.out.println("Maximum value is : " +c);
+
+rect r = new rect();      // calling rect class 
+r.area(2,3);
+ 
+value(2000.5);              // calling another method
+ 
 }
 
 //creating maxvalue function
 
-public static int maxvalue(int n1,int n2){
+public static int maxvalue(int n1,int n2){      // having return type int
 int max ;
 if (n1>n2)
 max = n1;
 else
 max = n2;
 return max;
-
+}
 /*public static int maxvalue(int a,int b){
 int max ;
 if (a>b)
@@ -23,6 +29,34 @@ max = a;
 else
 max = b;
 return max;*/
+
+
+// creating another method
+public static void value(double points){
+	if(points >= 1000.5)
+		System.out.println("A");
+	else if(points <= 1000.5)
+		System.out.println("Null");
 }
 
+
+
 }
+
+
+
+
+
+
+
+
+// creating another class and call it into main class
+class rect {
+	
+	public static void area(int x, int y) {
+		System.out.println("Area of rectangle is : " +x*y);
+	}
+}
+
+
+
