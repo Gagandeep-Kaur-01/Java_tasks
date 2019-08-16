0d7c4dab3f2a4a1bbe5d6bf458b18988e91983cd
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-08-16 21:36:57 +0530

    Enter no. of rows and no. of columns

---
+++ Eclipse_learning/src/MatrixProgram.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class MatrixProgram {
+
+	public static void main(String[]args) {
+		System.out.println("Enter the no. of rows in the matrix");
+			Scanner sc = new Scanner(System.in);
+		int rows = sc.nextInt();	
+		
+		System.out.println("Enter the no. of columns in the matrix");
+		int columns = sc.nextInt();	
+	}
+}
