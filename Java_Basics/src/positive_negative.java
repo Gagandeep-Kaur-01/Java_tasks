121757ca91415c325edc36c68a156a2578c0efd8
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-08-29 22:11:05 +0530

    to check no.is +ve or -ve or zero

---
+++ Java_Basics/src/positive_negative.java
@@ -0,0 +1,16 @@
+
+public class positive_negative {
+	public static void main(String[]args) {
+		int number = 0;
+		if (number>0) {
+			System.out.println("Number is positive" +number);
+		}
+		else if (number<0) {
+			System.out.println("Number is negative" +number);
+		}
+		else {
+			System.out.println("Number is zero");
+		}
+	}
+
+}
