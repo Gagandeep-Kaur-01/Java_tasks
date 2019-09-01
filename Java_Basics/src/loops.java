6bd640fcc0d264db4b4453f59cf763cfb01e7a54
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-09-01 15:32:29 +0530

    for-each loop

--- Java_Basics/src/loops.java
+++ Java_Basics/src/loops.java
@@ -12,6 +12,7 @@ public class loops {
 	
 	public static void main(String[]args) {
 		int count = 5;
+		
 		System.out.println("Use of for loop");
 		for(int i=1; i<=count;i++) {
 			for(int j=count; j>=i; j--) {
@@ -19,6 +20,15 @@ public class loops {
 				//System.out.println("*");
 		}
 		System.out.println(); // new line	
+		}
+	// ######################################################	
+		System.out.println('\n' +"Use of for-each loop");
+			/*for(Type var : array) {
+			 code to be executed 
+			  } */
+		int arr[]= {2,4,6,8,10}; //declaring an array
+		for(int i:arr) {  //printing an array using for-each loop
+			System.out.println(i);
 		}
 	}
 }
